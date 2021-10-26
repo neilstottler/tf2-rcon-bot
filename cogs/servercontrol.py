50294677b8b22e71a3bf346354553705fa2858a2
neilstottler <2000nesman@gmail.com>
2021-10-26 10:02:07 -0400

    IT WORKS

--- cogs/servercontrol.py
+++ cogs/servercontrol.py
@@ -2,11 +2,7 @@ import discord
 from discord.ext.commands import Cog, command, has_any_role
 from utils import load_config
 import asyncssh
-import subprocess
-import os
-import nmap
-
-
+import asyncio
 
 global_config = load_config()
 config = global_config.sftp
@@ -21,26 +17,33 @@ class server(Cog):
         servers = ["eu", "us", "eumvm", "usmvm"]
         commands = ["start", "stop", "restart", "update"]
 
-        if server in servers:
-            #eu
-            if server == "eu":
-                await ctx.send(server_connection("tf", "eu.tf2maps.net", 25015, command))
-            #us
-            elif server == "us":
-                await ctx.send(server_connection("tf", "us.tf2maps.net", 25015, command))
-            #eumvm
-            elif server == "eumvm":
-                await ctx.send(server_connection("mvm", "eu.tf2maps.net", 25016, command))
-            #usmvm
-            elif server == "usmvm":
-                await ctx.send(server_connection("mvm", "us.tf2maps.net", 25016, command))
+        if command in commands:
+            if server in servers:
+                #eu
+                if server == "eu":
+                    await ctx.send(await server_connection("tf", "eu.tf2maps.net", 22, command))
+                #us
+                elif server == "us":
+                    await ctx.send(await server_connection("tf", "us.tf2maps.net", 22, command))
+
+                #eumvm
+                elif server == "eumvm":
+                    await ctx.send(await server_connection("mvm", "eu.tf2maps.net", 22, command))
+
+                #usmvm
+                elif server == "usmvm":
+                    await ctx.send(await server_connection("mvm", "us.tf2maps.net", 22, command))
+
+                else:
+                    await ctx.send("How did it reach this part?")
 
             else:
-                await ctx.send("How did it reach this part?")
-        else:
-            await ctx.trigger_typing()
-            await ctx.send("Server not found. Check `?servers`.")
+                await ctx.trigger_typing()
+                await ctx.send("Server not found. Check `?servers`.")
 
+        else:
+            await ctx.send("Invalid command argument. See ?commands.")
+        
     #command to see commands for ?server
     @command()
     @has_any_role('Staff', 'Server Mods', 'Senior Staff', 'Fub')
@@ -55,36 +58,36 @@ class server(Cog):
         await ctx.send(embed=embed)
 
 #server connecting
-async def server_connection(username, hostname, port, command):
+async def server_connection(user, hostname, port, command):
 
     commands = ["start", "stop", "restart", "update"]
 
     async with asyncssh.connect(
-        host=hostname,
+        hostname,
         port=port, 
-        username=username, 
+        username=user, 
         password=config.master.password,
         known_hosts=None
     ) as conn:
         if command in commands:
             if command == "start":
 
-                conn.run('./server start')
+                await conn.run('./server start')
                 return "Server starting."
 
             elif command == "stop":
 
-                conn.run('./server stop')
+                await conn.run('./server stop')
                 return "Stopping server."
  
             elif command == "restart":
 
-                conn.run('./server restart')                                
+                await conn.run('./server restart')                                
                 return "Restarting server"
 
             elif command == "update":
                 
-                conn.run('./server update')
+                await conn.run('./server update')
                 return "Updating server."
 
             else:
