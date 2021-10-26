72e6266691ca79df31926466d18e9b917363ba90
neilstottler <2000nesman@gmail.com>
2021-10-26 10:37:31 -0400

    added server check

--- cogs/servercontrol.py
+++ cogs/servercontrol.py
@@ -1,8 +1,9 @@
+import asyncio
+import socket
 import discord
 from discord.ext.commands import Cog, command, has_any_role
 from utils import load_config
 import asyncssh
-import asyncio
 
 global_config = load_config()
 config = global_config.sftp
@@ -21,18 +22,18 @@ class server(Cog):
             if server in servers:
                 #eu
                 if server == "eu":
-                    await ctx.send(await server_connection("tf", "eu.tf2maps.net", 22, command))
+                    await ctx.send(await server_connection("tf", "eu.tf2maps.net", 27015, command))
                 #us
                 elif server == "us":
-                    await ctx.send(await server_connection("tf", "us.tf2maps.net", 22, command))
+                    await ctx.send(await server_connection("tf", "us.tf2maps.net", 27015, command))
 
                 #eumvm
                 elif server == "eumvm":
-                    await ctx.send(await server_connection("mvm", "eu.tf2maps.net", 22, command))
+                    await ctx.send(await server_connection("mvm", "eu.tf2maps.net", 27016, command))
 
                 #usmvm
                 elif server == "usmvm":
-                    await ctx.send(await server_connection("mvm", "us.tf2maps.net", 22, command))
+                    await ctx.send(await server_connection("mvm", "us.tf2maps.net", 27016, command))
 
                 else:
                     await ctx.send("How did it reach this part?")
@@ -57,6 +58,12 @@ class server(Cog):
 
         await ctx.send(embed=embed)
 
+    #ping gameserver
+    @command()
+    @has_any_role('Staff', 'Server Mods', 'Senior Staff', 'Fub')
+    async def ping(self, ctx):
+        pass
+
 #server connecting
 async def server_connection(user, hostname, port, command):
 
@@ -64,7 +71,7 @@ async def server_connection(user, hostname, port, command):
 
     async with asyncssh.connect(
         hostname,
-        port=port, 
+        port=22, 
         username=user, 
         password=config.master.password,
         known_hosts=None
@@ -73,25 +80,66 @@ async def server_connection(user, hostname, port, command):
             if command == "start":
 
                 await conn.run('./server start')
-                return "Server starting."
+
+                #check if server is up
+                await asyncio.sleep(5)
+                if await check_port(hostname, port):
+                    return "Server started."
+                else:
+                    return "Server not started."
 
             elif command == "stop":
 
                 await conn.run('./server stop')
-                return "Stopping server."
+
+                #check if server is running
+                await asyncio.sleep(5)
+                if await check_port(hostname, port):
+                    return "Server not stopped."
+                else:
+                    return "Server is stopped."
  
             elif command == "restart":
 
-                await conn.run('./server restart')                                
-                return "Restarting server"
+                #stop server and update
+                await conn.run('./server stop') 
+                await asyncio.sleep(5)
+                await conn.run('./server update')
+
+                #check if server is up
+                await asyncio.sleep(8)
+                if await check_port(hostname, port):
+                    return "Server restarted."
+                else:
+                    return "Server not restarted."
 
             elif command == "update":
                 
                 await conn.run('./server update')
-                return "Updating server."
+
+                #check if server is up
+                await asyncio.sleep(10)
+                if await check_port(hostname, port):
+                    return "Server online and updated."
+                else:
+                    return "Server offline."
 
             else:
                 return "How did you manage to break this after a checksum?" 
 
         else:
             return "Invalid command. Check `?commands`."
+
+#semi dirty way to check if the server is up
+async def check_port(hostname, port):
+    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    location = (hostname, port)
+
+    result_of_check = a_socket.connect_ex(location)
+
+    if result_of_check == 0:
+        return True
+    else:
+        return False
+    pass
